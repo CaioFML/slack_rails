f930bc81d82a8022153e27e363e61cff68bd7438
CaioFML <caio.felipemotta@hotmail.com>
2020-03-20 16:57:18 -0300

    Controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'channels/create'
+
+  get 'channels/destroy'
+
+  get 'channels/show'
+
+  get 'teams/index'
+
+  get 'teams/show'
+
+  get 'teams/create'
+
+  get 'teams/destroy'
+
   devise_for :users, controllers: { registrations: 'registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
