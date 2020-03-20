e27c6b6b2477ae9879556b2c41bc3bd91ff73638
CaioFML <caio.felipemotta@hotmail.com>
2020-03-20 14:33:59 -0300

    Criando nossos Factories (Altera para FactoryBot)

---
+++ spec/factories/channels.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :channel do
+    slug { FFaker::Lorem.word }
+    team
+    user { team.user }
+  end
+end
---
+++ spec/factories/messages.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :message do
+    body {FFaker::Lorem.sentence}
+    user
+  end
+end
