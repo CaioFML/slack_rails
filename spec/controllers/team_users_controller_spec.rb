2cb05ddcb6738e538a1131e11448ab029abec45e
CaioFML <caio.felipemotta@hotmail.com>
2020-04-06 17:54:29 -0300

    Ajustando os Retornos

--- spec/controllers/team_users_controller_spec.rb
+++ spec/controllers/team_users_controller_spec.rb
@@ -7,20 +7,32 @@ RSpec.describe TeamUsersController, type: :controller do
     request.env["HTTP_ACCEPT"] = 'application/json'
 
     @request.env["devise.mapping"] = Devise.mappings[:user]
-    @current_user = create(:user)
+    @current_user = FactoryGirl.create(:user)
     sign_in @current_user
   end
 
   describe "GET #crete" do
+    # Sem isto os testes não renderizam o json
+    render_views
+
     context "Team owner" do
       before(:each) do
         @team = create(:team, user: @current_user)
         @guest_user = create(:user)
+
+        post :create, params: { team_user: { email: @guest_user.email, team_id: @team.id } }
       end
 
       it "returns http success" do
-        post :create, params: { team_user: { user_id: @guest_user.id, team_id: @team.id } }
         expect(response).to have_http_status(:success)
+      end
+
+      it "Return the right params" do
+        response_hash = JSON.parse(response.body)
+
+        expect(response_hash["user"]["name"]).to eql(@guest_user.name)
+        expect(response_hash["user"]["email"]).to eql(@guest_user.email)
+        expect(response_hash["team_id"]).to eql(@team.id)
       end
     end
 
@@ -31,7 +43,7 @@ RSpec.describe TeamUsersController, type: :controller do
       end
 
       it "returns http forbidden" do
-        post :create, params: { team_user: { user_id: @guest_user.id, team_id: @team.id } }
+        post :create, params: { team_user: { email: @guest_user.email, team_id: @team.id } }
         expect(response).to have_http_status(:forbidden)
       end
     end
